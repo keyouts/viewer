28fd51821137631ddd3178390bed710b0584df69
keyouts <keyouts@outlook.com>
2025-07-22 09:55:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ document.addEventListener("DOMContentLoaded", () => {
           link: "https://www.recordsmithofpennsylvania.com/"
         },
         {
-          src: "https://images.squarespace-cdn.com/content/686b137649e6ef6f1bbbe325/85df8771-7533-43ac-ad8c-ee051e5ddecb/MoffittSmith.png?content-type=image%2Fpng",
-          link: "https://www.moffitt-smith.net/"
+          src: "https://images.squarespace-cdn.com/content/686b137649e6ef6f1bbbe325/0c26bc8a-e48b-44e4-84d2-a1b8a43602e7/WhimsyAHairSalon.png?content-type=image%2Fpng",
+          link: "https://whimsy-ahairsalon.square.site/"
         },
         {
-              src: "https://images.squarespace-cdn.com/content/686b137649e6ef6f1bbbe325/0c26bc8a-e48b-44e4-84d2-a1b8a43602e7/WhimsyAHairSalon.png?content-type=image%2Fpng",
-          link: "https://whimsy-ahairsalon.square.site/"
+                        src: "https://images.squarespace-cdn.com/content/686b137649e6ef6f1bbbe325/85df8771-7533-43ac-ad8c-ee051e5ddecb/MoffittSmith.png?content-type=image%2Fpng",
+          link: "https://www.moffitt-smith.net/"
         }
 
       ];
