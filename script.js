4d994720805c9267f79b4c69d412750a433e6ca2
keyouts <keyouts@outlook.com>
2025-07-25 18:59:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
       const slides = [
         {
                   src: "https://images.squarespace-cdn.com/content/686b137649e6ef6f1bbbe325/85df8771-7533-43ac-ad8c-ee051e5ddecb/MoffittSmith.png?content-type=image%2Fpng",
-          link: "https://www.moffitt-smith.net/"
         },
         {
           src: "https://images.squarespace-cdn.com/content/686b137649e6ef6f1bbbe325/0c26bc8a-e48b-44e4-84d2-a1b8a43602e7/WhimsyAHairSalon.png?content-type=image%2Fpng",
-          link: "https://whimsy-ahairsalon.square.site/"
+
         },
         {    
               src: "https://images.squarespace-cdn.com/content/686b137649e6ef6f1bbbe325/199b639e-f4ca-4e30-a5e3-3478a9bc5891/RecordSmith.png?content-type=image%2Fpng",
-          link: "https://www.recordsmithofpennsylvania.com/"
         }
 
       ];
