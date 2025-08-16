224eb64aa35bcf0ae7a2fdc8d4e91f408ea20bc8
keyouts <admin@keyouts.com>
2025-08-16 15:14:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const slides = [
-    {
-      src: "https://images.squarespace-cdn.com/content/686b137649e6ef6f1bbbe325/85df8771-7533-43ac-ad8c-ee051e5ddecb/MoffittSmith.png?content-type=image%2Fpng",
-    },
-      {
-      src: "https://images.squarespace-cdn.com/content/686b137649e6ef6f1bbbe325/20e0e43c-665f-4130-8646-e575336da484/WhimsyAHairSalon.jpg?content-type=image%2Fjpeg",
-    },
-    {
-      src: "https://images.squarespace-cdn.com/content/686b137649e6ef6f1bbbe325/199b639e-f4ca-4e30-a5e3-3478a9bc5891/RecordSmith.png?content-type=image%2Fpng",
-    },
-    {
-      src: "https://images.squarespace-cdn.com/content/686b137649e6ef6f1bbbe325/3d60cdfa-1484-40f4-9e64-01dd1cbc8461/KeystoneILogo.png?content-type=image%2Fpng",
-    }
-  ];
+ const slides = [
+  {
+    src: "https://images.squarespace-cdn.com/content/686b137649e6ef6f1bbbe325/85df8771-7533-43ac-ad8c-ee051e5ddecb/MoffittSmith.png?content-type=image%2Fpng",
+    alt: "Logo for Moffitt-Smith, a furniture wholesaler."
+  },
+  {
+    src: "https://images.squarespace-cdn.com/content/686b137649e6ef6f1bbbe325/20e0e43c-665f-4130-8646-e575336da484/WhimsyAHairSalon.jpg?content-type=image%2Fjpeg",
+    alt: "Logo for Whimsy: A Hair Salon."
+  },
+  {
+    src: "https://images.squarespace-cdn.com/content/686b137649e6ef6f1bbbe325/199b639e-f4ca-4e30-a5e3-3478a9bc5891/RecordSmith.png?content-type=image%2Fpng",
+    alt: "Logo for Record Smith, a record store."
+  },
+  {
+    src: "https://images.squarespace-cdn.com/content/686b137649e6ef6f1bbbe325/3d60cdfa-1484-40f4-9e64-01dd1cbc8461/KeystoneILogo.png?content-type=image%2Fpng",
+    alt: "Logo for Keystone I, a tarot reader.
+  }
+];
+
+
 
   let current = 0;
   const imgEl = document.getElementById("carousel-image");
